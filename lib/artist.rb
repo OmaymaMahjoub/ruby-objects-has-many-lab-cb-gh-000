91736b2095971d0e86650d4286b3f64fe01ef200
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 14:29:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+
+  @@songs=[]
+
+  def initialize (name)
+    @name=name
+  end
+
+  attr_accessor :name
+
+  def self.add_song(song)
+    @@songs.push(song)
+  end
+
+  def self.add_song_by_name(name)
+    @@songs<<Song.new(name)
+  end
+
+  def self.song_count
+    @@songs.size
+  end
+  
+end
